2129c423b2dc6f44202645bfe9244d61edf1ade7
yash-2115 <mailforyash04@gmail.com>
2022-11-27 15:49:15 +0530

    var, let and const Done

---
+++ 003_let_var_and_const/let_var_const.js
@@ -0,0 +1,20 @@
+// ------------------------- globally defined ---------------------
+// var a = 'yash'
+// {
+//     var a = 'yash patel'
+//     console.log(a)
+// }
+// console.log(a)
+
+// ------------------ Block scope -----------
+// let a = 'yash'
+// {
+//     let a = 'yash patel'
+//     console.log(a)
+// }
+// console.log(a)
+
+// ---------- Const ------------
+const author = 'Yash'
+// let author = 'Yash' // Will throw an error because constant can not be changed
+// author = 'Dhruv'
