3795456d4e983aa5372260a0f2c4d4b79eec7c3b
yash-2115 <mailforyash04@gmail.com>
2022-11-27 16:14:22 +0530

    Primitive and Objects

---
+++ 004_Primitive_and_objects/1_primitive.js
@@ -0,0 +1,11 @@
+let a = null
+let b = 345
+let c = true
+let d = BigInt("1525")
+let e = "Yash"
+let f = Symbol('Yash is a good symbol')
+let g = undefined
+// let g --> this will be undefined automatically
+
+console.log(a, b, c, d, e, f, g)
+console.log(typeof g);
---
+++ 004_Primitive_and_objects/2_objects.js
@@ -0,0 +1,12 @@
+// Non Primitive datatype - Objects in js
+
+const item = {
+    "Yash": true,
+    "Dhruv": false,
+    "Drishti": 24,
+    "Rita": 21
+}
+
+console.log(item);
+console.log(item["Yash"]); // Will return true
+console.log(item["adfsdf"]); // Will return undefined
