0a0fea6d080d6646c31dabdfaa6db943776b9d05
yash-2115 <mailforyash04@gmail.com>
2022-11-28 08:00:11 +0530

    While Loop and Do While Loop

---
+++ 009_while_loop/1_while_loop.js
@@ -0,0 +1,7 @@
+n = 10
+i = 0
+
+while (i < n) {
+    console.log(i);
+    i++;
+}
---
+++ 009_while_loop/2_do_while_loop.js
@@ -0,0 +1,9 @@
+n = 10
+i = 0
+
+// This loop will execute atlast once at any cost
+// Here in do while loop first loop will execute then it will check conditions.
+do {
+    console.log(i);
+    i++;
+} while (i < n)
