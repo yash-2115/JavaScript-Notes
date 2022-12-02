d10e3e8039b378d086e995988a2141bda9d71aea
yash-2115 <mailforyash04@gmail.com>
2022-12-02 21:51:20 +0530

    CH - 5 Practice

--- Practice/CH - 5/solution.js
+++ Practice/CH - 5/solution.js
@@ -1,9 +1,47 @@
 // Q1
 // Create an array of number and take input from the user to add numbers to this array.
+// let arr = [1, 2, 4, 5, 6, 7]
+// let a = prompt("Enter the number")
+// a = Number.parseInt(a)
+// arr.push(a)
+// console.log(arr);
 
-let arr = [1, 2, 4, 5, 6, 7]
-let a = prompt("Enter the number")
+// Q2
+// keep adding numbers to the array in question 1 untill 0 is added to the array.
 
-a = Number.parseInt(a)
-arr.push(a)
-console.log(arr);
+// let arr = [1, 2, 4, 5, 6, 7]
+// let a;
+// do {
+//     a = prompt("Enter the number")
+//     a = Number.parseInt(a)
+//     arr.push(a)
+//     console.log(arr); 
+// } while (a != 0) {
+
+// }
+
+// Q3
+// filter for number divisible by 10 from a given array.
+
+// let arr = [1, 2, 40, 50, 6, 7]
+// let n = arr.filter((x) => {
+//     return x%10 == 0
+// })
+// console.log(n);
+
+// Q4
+// Create an array of square of given number
+// let arr = [1, 2, 40, 50, 6, 7]
+// let n = arr.map((x) => {
+//     return x*x
+// })
+// console.log(n);
+
+// Q5
+// Use reduce to calculate factorial of a given number from an array of first n natural number (n bing the number whose factorial needs to be calculated)
+
+// let arr = [1, 2, 3, 4]
+// let n = arr.reduce((x1, x2) => {
+//     return x1 * x2
+// })
+// console.log(n);
