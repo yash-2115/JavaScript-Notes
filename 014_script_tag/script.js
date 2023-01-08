2e25c3aaf1e90258fcbcbe786191ee625d011358
yash-2115 <mailforyash04@gmail.com>
2023-01-08 12:13:26 +0530

    Console objects

---
+++ 014_script_tag/script.js
@@ -0,0 +1,28 @@
+// console.assert()
+console.assert(5>10)
+
+// console.clear()
+console.clear()
+
+// console.log()
+console.log()
+
+// console.table()
+obj = {a:1, b:2, c:3}
+console.table(obj)
+
+// console.warn()
+console.warn("Please do not drink much daru")
+
+// console.error()
+console.error("Yeah kya kar raha he tu ? Nikal is website se")
+
+// console.info
+console.info("Yash is good boy")
+
+// console.time()
+console.time("forloop")
+for(let i=0;i<5;i++){
+    console.log(233)
+}
+console.timeEnd("forloop")
